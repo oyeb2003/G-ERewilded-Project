085a956931c25696532c815a9a7ac4485d192329
oyeb2003 <79373782+oyeb2003@users.noreply.github.com>
2024-04-28 19:43:00 -0400

    Add files via upload

--- Bola_PCA_Density_use this_gen+env.R
+++ Bola_PCA_Density_use this_gen+env.R
@@ -10,21 +10,28 @@ library(ggsci)
 library(ggsignif)
 
 
-setwd("/Volumes/oyesolaoo$/My Documents/Rewilding Project/Flow Cytometry/Blood Analysis Unsupervised")
+setwd("/Volumes/oyesolaoo$/My Documents/Rewilding Project/Flow Cytometry/MLN Analysis Unsupervised")
 
-metadata = read.csv("FACS_Blood_Unsupervised_Labelled1.csv",header = T, row.names = 1)
+metadata = read.csv("MLN_ICS_Compositional data_named.csv",header = T, row.names = 1)
 head(metadata)
-colnames(metadata)
 
-input = metadata[,c(7:21)]
-input 
-metadata[, 2:4] <- lapply(metadata[,2:4], as.factor)
-effectors= metadata[,2:4]
+input = metadata[,c(4:18)]
+input
+metadata[, 1:3] <- lapply(metadata[,1:3], as.factor)
+effectors= metadata[,1:3]
 effectors
 
   D <- dist(input, method = "euclidean")
   res <- pcoa(D)
-  plotter <- data.frame(res$vectors, col_var=metadata$Genotype, shape_var=metadata$Environment)
+  print(res)
+  class(res)
+  str(res)
+  
+  sum <- as.data.frame(res$vectors)
+  sum
+  write.csv(sum, "PCA_April.csv")
+  
+  plotter <- data.frame(res$vectors, col_var=metadata$Strain, shape_var=metadata$Location)
   
   
   
@@ -103,18 +110,18 @@ effectors
   g_seg2
   
   g_seg=ggplot(plotter, aes(Axis.1, Axis.2)) +
-    geom_point(size=20, alpha=0) + ggtitle("Loadings for Clusters") +
+    geom_point(size=3, alpha=0) + ggtitle("Loadings for Clusters") +
     ylab(paste0("PCo2 ", round(res$values$Broken_stick[2]*100,2), "% expl. variation")) +
     xlab(paste0("PCo1 ", round(res$values$Broken_stick[1]*100,2), "% expl. variation")) +
     geom_segment(data=datapc2, aes(x=0, y=0, xend=Axis.1, yend=Axis.2), 
                  arrow=arrow(length=unit(0.2,"cm")), alpha=0.5) +
     geom_label_repel(data=datapc2, aes(x=Axis.1, y=Axis.2, label=varnames), 
-                     size = 6, force=10, segment.alpha=0.5) +
-  theme_void()
+                     size = 4, force=4, segment.alpha=0.5) +
+    theme_void() 
   g_seg
   
   
-  pc1_hist <- ggplot(plotter, aes(Axis.1, color = shape_var, fill= shape_var)) +
+  pc1_hist <- ggplot(plotter, aes(Axis.1, color =  col_var, fill= col_var)) +
     geom_density(alpha=0.6) + 
     scale_y_reverse() +
     scale_color_manual(values = c("mediumpurple1", "red3","forestgreen")) +
@@ -122,7 +129,7 @@ effectors
     theme_void() + theme(legend.position = "none")
   pc1_hist
     
-  pc2_hist <- ggplot(plotter, aes(Axis.2, color = col_var, fill= col_var)) +
+  pc2_hist <- ggplot(plotter, aes(Axis.2, color = shape_var, fill= shape_var)) +
     geom_density(alpha=0.6) + 
     coord_flip() +
     scale_y_reverse() +
